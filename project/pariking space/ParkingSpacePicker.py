21c92e249f2c51dfd544384466000d590bfea0b5
omid jalilian <omidjalilian147@gmail.com>
2023-10-16 18:55:58 +0330

    project

--- project/pariking space/ParkingSpacePicker.py
+++ project/pariking space/ParkingSpacePicker.py
@@ -1,11 +1,5 @@
-# import image of parking space
-# mark every parking space as Region Of Interest
-# works with fixed camera
 import cv2
 import pickle
-
-# store clicked rectangles
-
 
 width, height = 107, 48
 
@@ -27,11 +21,7 @@ def mouseClick(events, x, y, flags, params):
     with open('CarParkPos', 'wb') as f:
         pickle.dump(posList, f)
 
-
 while True:
-    # create rectangle in exact positions
-    # cv2.rectangle(img,(50,192),(157,240),(255,0,255),2)
-
     img = cv2.imread("./assets/carParkImg.png")
     for pos in posList:
         cv2.rectangle(img, pos, (pos[0]+width, pos[1]+height),(255,0,255),2)
